e5b79958b88bc6f2adcd6f6b94fabe4362945826
eniepa <apeine.k@gmail.com>
2020-06-27 16:23:22 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzle15
+{
+    public partial class PuzzleArea : Form
+    {
+        public PuzzleArea()
+        {
+            InitializeComponent();
+            InitializePuzzleArea();
+        }
+
+        private void InitializePuzzleArea()
+        {
+            this.BackColor = Color.Purple;
+            this.Text = "puzzle15";
+        }
+
+        private void InitializeButtons()
+        {
+            Button button;
+
+        }
+    }
+}
