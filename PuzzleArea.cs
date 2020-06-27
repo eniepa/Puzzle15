690cc5eda5ba6b93c616e5de2551b7892f217f24
eniepa <apeine.k@gmail.com>
2020-06-27 17:05:52 +0300

    27.06

---
+++ PuzzleArea.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzle15
+{
+    public partial class PuzzleArea : Form
+    {
+        public PuzzleArea()
+        {
+            InitializeComponent();
+            InitializePuzzleArea();
+            InitializeBlocks();
+        }
+
+        private void InitializePuzzleArea()
+        {
+            this.BackColor = Color.CornflowerBlue;
+            this.Text = "puzzle15";
+            this.ClientSize = new Size(450, 450);
+        }
+
+        private void InitializeBlocks()
+        {
+            int blockCount = 1;
+            PuzzleBlock block;
+            for (int row = 1; row < 5; row++)
+            {
+                for (int col = 1; col < 5; col++)
+                {
+                    block = new PuzzleBlock();
+                    block.Top = row * 85;
+                    block.Left = col * 85;
+                    block.Text = blockCount.ToString();
+                    blockCount++;
+                    this.Controls.Add(block);
+                }
+
+            }
+        }
+    }
+}
---
+++ PuzzleBlock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace puzzle15 
+{
+    class PuzzleBlock : Button
+    {
+        public PuzzleBlock()
+        {
+            InitializePuzzleBlock();
+        }
+
+        private void InitializePuzzleBlock()
+        {
+            this.Height = 70;
+            this.Width = 70;
+            this.BackColor = Color.LimeGreen;
+            this.Text = "0";
+            this.Font = new Font("Roboto", 18);
+        }
+    }
+}
